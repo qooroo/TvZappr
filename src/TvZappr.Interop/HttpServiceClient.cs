78b1acd08bfa84f2807903992c1434a290cebd71
Qiming Liu <qooroo@gmail.com>
2015-02-23 14:34:59 +0000

    add basic smoke test

---
+++ src/TvZappr.Interop/HttpServiceClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace TvZappr.Interop
+{
+    public interface IHttpServiceClient
+    {
+        Task<TResponse> GetAsync<TResponse>(string resource);
+        Task<TResponse> PostAsync<TRequest, TResponse>(string resource, TRequest request);
+    }
+
+    public class HttpServiceClient : IHttpServiceClient
+    {
+        private readonly IUrlProvider _urlProvider;
+        private readonly Func<HttpClient> _httpClient;
+
+        public HttpServiceClient(IUrlProvider urlProvider)
+        {
+            _urlProvider = urlProvider;
+            _httpClient = () =>
+            {
+                var client = new HttpClient { BaseAddress = new Uri(_urlProvider.WebServiceUrl) };
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return client;
+            };
+        }
+
+        public async Task<TResponse> GetAsync<TResponse>(string resource)
+        {
+            using (var client = _httpClient())
+            {
+                var response = await client.GetAsync(resource);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsAsync<TResponse>();
+            }
+        }
+
+        public async Task<TResponse> PostAsync<TRequest, TResponse>(string resource, TRequest request)
+        {
+            using (var client = _httpClient())
+            {
+                var response = await client.PostAsJsonAsync(resource, request);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsAsync<TResponse>();
+            }
+        }
+    }
+}
---
+++ src/TvZappr.Tests/ServiceSmokeTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using FluentAssertions;
+using TvZappr.Interop;
+using Xunit;
+
+namespace TvZappr.Tests
+{
+    public class ServiceSmokeTests
+    {
+        private readonly HttpServiceClient _httpClient;
+        private const string ServiceExe = @"..\..\..\TvZAppr.Web\bin\Debug\TvZappr.Web.exe";
+
+        public ServiceSmokeTests()
+        {
+            _httpClient = new HttpServiceClient(new TestUrlProvider());
+        }
+
+        [Fact]
+        public void GivenServiceRunning_ThenPingControllerRespondsOk()
+        {
+            Process svc = null;
+            try
+            {
+                svc = Process.Start(ServiceExe);
+                Thread.Sleep(1000);
+                _httpClient.GetAsync<string>(ApiRoutes.Ping).Result.Should().Be("pong!");
+            }
+            finally
+            {
+                if (svc != null)
+                {
+                    svc.Kill();
+                }
+            }
+        }
+    }
+}
